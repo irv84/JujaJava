dab361780aa8dcbe7bce61d4f864a6dea2df6196
irv84 <ignatenkoroman@gmail.com>
2018-08-29 18:50:16 +0300

    Chapter1. Lab1

---
+++ src/ua/com/juja/core/Chapter1/Lab1/JujaAwesome.java
@@ -0,0 +1,11 @@
+package ua.com.juja.core.Chapter1.Lab1;
+
+//В этом задании тебе необходимо напечатать в консоль фразу "Juja is awesome!".
+
+
+
+public class JujaAwesome {
+    public static void main(String[] args) {
+        System.out.println("Juja is awesome!");
+    }
+}
